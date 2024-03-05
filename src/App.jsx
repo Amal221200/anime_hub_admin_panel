c96d0257e35144181e9c467cc428f27c698d35ee
Amal221200 <amalmurikkoli2000@gmail.com>
2024-03-05 11:03:38 +0530

    little changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,63 +3,64 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layout/MainLayout";
 import { lazy, Suspense } from "react";
 import AuthLayout from "./layout/AuthLayout";
+
 const SignInPage = lazy(() => import("./pages/SignInPage"));
 const EditAnimePage = lazy(() => import("./pages/EditAnimePage"));
 const AddAnimePage = lazy(() => import("./pages/AddAnimePage"));
 const MerchandisePage = lazy(() => import("./pages/MerchandisePage"));
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <AuthLayout />,
-        children: [
-            {
-                path: "/sign-in",
-                element: <Suspense><SignInPage /></Suspense>
-            },
-            // {
-            //     path: "/sign-up",
-            //     element: <SignUpPage />
-            // },
-            {
-                path: "/",
-                element: <MainLayout />,
-                children: [
-                    {
-                        path: "/",
-                        element: <HomePage />
-                    },
-                    {
-                        path: "/anime",
-                        element: (
-                            <Suspense>
-                                <AddAnimePage />
-                            </Suspense>
-                        )
-                    },
-                    {
-                        path: "/anime/:id",
-                        element: (
-                            <Suspense>
-                                <EditAnimePage />
-                            </Suspense>
-                        )
-                    },
-                    {
-                        path: "/merchandise",
-                        element: (
-                            <Suspense>
-                                <MerchandisePage />
-                            </Suspense>
-                        )
-                    },
-                ]
-            }]
-    },
-])
+
 
 export default function App() {
-
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <AuthLayout />,
+            children: [
+                {
+                    path: "/sign-in",
+                    element: <Suspense><SignInPage /></Suspense>
+                },
+                // {
+                //     path: "/sign-up",
+                //     element: <SignUpPage />
+                // },
+                {
+                    path: "/",
+                    element: <MainLayout />,
+                    children: [
+                        {
+                            path: "/",
+                            element: <HomePage />
+                        },
+                        {
+                            path: "/anime",
+                            element: (
+                                <Suspense>
+                                    <AddAnimePage />
+                                </Suspense>
+                            )
+                        },
+                        {
+                            path: "/anime/:id",
+                            element: (
+                                <Suspense>
+                                    <EditAnimePage />
+                                </Suspense>
+                            )
+                        },
+                        {
+                            path: "/merchandise",
+                            element: (
+                                <Suspense>
+                                    <MerchandisePage />
+                                </Suspense>
+                            )
+                        },
+                    ]
+                }]
+        },
+    ], {})
     return (
         <RouterProvider router={router} />
     )
--- src/lib/ApiFetcher.js
+++ src/lib/ApiFetcher.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const ApiFetcher = axios.create({
-    baseURL:import.meta.env.VITE_SERVER_URL,
+    baseURL: import.meta.env.VITE_SERVER_URL,
     withCredentials: true
 })
 
-export default ApiFetcher
+export default ApiFetcher;
