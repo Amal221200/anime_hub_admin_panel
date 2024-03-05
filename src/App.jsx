0ef1e94d36e002593e39b7c4b3867820a3a1c6b5
Amal221200 <amalmurikkoli2000@gmail.com>
2024-03-05 12:04:15 +0530

    vercel

--- src/App.jsx
+++ src/App.jsx
@@ -9,58 +9,53 @@ const EditAnimePage = lazy(() => import("./pages/EditAnimePage"));
 const AddAnimePage = lazy(() => import("./pages/AddAnimePage"));
 const MerchandisePage = lazy(() => import("./pages/MerchandisePage"));
 
-
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <AuthLayout />,
+        children: [
+            {
+                path: "/sign-in",
+                element: <Suspense><SignInPage /></Suspense>
+            },
+            {
+                path: "/",
+                element: <MainLayout />,
+                children: [
+                    {
+                        path: "/",
+                        element: <HomePage />
+                    },
+                    {
+                        path: "/anime",
+                        element: (
+                            <Suspense>
+                                <AddAnimePage />
+                            </Suspense>
+                        )
+                    },
+                    {
+                        path: "/anime/:id",
+                        element: (
+                            <Suspense>
+                                <EditAnimePage />
+                            </Suspense>
+                        )
+                    },
+                    {
+                        path: "/merchandise",
+                        element: (
+                            <Suspense>
+                                <MerchandisePage />
+                            </Suspense>
+                        )
+                    },
+                ]
+            }]
+    },
+])
 
 export default function App() {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <AuthLayout />,
-            children: [
-                {
-                    path: "/sign-in",
-                    element: <Suspense><SignInPage /></Suspense>
-                },
-                // {
-                //     path: "/sign-up",
-                //     element: <SignUpPage />
-                // },
-                {
-                    path: "/",
-                    element: <MainLayout />,
-                    children: [
-                        {
-                            path: "/",
-                            element: <HomePage />
-                        },
-                        {
-                            path: "/anime",
-                            element: (
-                                <Suspense>
-                                    <AddAnimePage />
-                                </Suspense>
-                            )
-                        },
-                        {
-                            path: "/anime/:id",
-                            element: (
-                                <Suspense>
-                                    <EditAnimePage />
-                                </Suspense>
-                            )
-                        },
-                        {
-                            path: "/merchandise",
-                            element: (
-                                <Suspense>
-                                    <MerchandisePage />
-                                </Suspense>
-                            )
-                        },
-                    ]
-                }]
-        },
-    ], { basename: import.meta.env?.VITE_BASE_URL || "/" })
     return (
         <RouterProvider router={router} />
     )
